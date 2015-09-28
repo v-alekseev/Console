35b99a97f0a0419c8f8d08a4bbbdff8e0e41e45a
Vitaly <valekseev@yandex.ru>
2015-09-29 00:20:56 +0300

    Add text "Second line"

--- src/com/vitalik/Main.java
+++ src/com/vitalik/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.printf("Test console application");
-
+        System.out.println("Test console application");
+        System.out.println("Second line");
 
     }
 }
