81882eefd798c00ec9013c193f701ddfe1420809
Vitaly <valekseev@yandex.ru>
2015-09-29 00:16:39 +0300

    init commit

---
+++ src/com/vitalik/Main.java
@@ -0,0 +1,11 @@
+package com.vitalik;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.printf("Test console application");
+
+
+    }
+}
