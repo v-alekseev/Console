bf3e62f83a363e3c4cec14106557752603eeacff
Vitaly <valekseev@yandex.ru>
2015-10-05 00:22:15 +0300

    Add read from console

--- src/com/vitalik/Main.java
+++ src/com/vitalik/Main.java
@@ -1,11 +1,21 @@
 package com.vitalik;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Test console application");
-        System.out.println("Second line");
+
+        String name;  // переменная для имени
+        Scanner in = new Scanner(System.in);
+       // name = con.readLine("Введите свое имя: ");
+        System.out.print("Введите свое имя: ");
+        name = in.nextLine();//считываем одну строку целиком
+
+        //System.out.println("Name - " + name);
+        System.out.println("Name - " + name);
 
     }
 }
